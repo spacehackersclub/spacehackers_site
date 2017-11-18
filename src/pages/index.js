c2d05a851d54194a6fa05b03b89d45f14a8e69f6
Kevin Ridgway <kridgway@gmail.com>
2017-11-18 11:42:47 -0500

    Basic content.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,27 @@
-import React from "react";
-import Link from "gatsby-link";
+import React from 'react';
+import Link from 'gatsby-link';
 
 const IndexPage = ({ data }) => (
-  <div>
-    <h3>Join Us!</h3>
-    <p>I am working on creating a community in which we can openly collobarate.</p> 
-  </div>
+	<div>
+		<h1>Join Us!</h1>
+		<h2>Early Beginnings...</h2>
+		<p>I am working on creating a community in which we can openly collobarate.</p>
+		<ul>
+			<li>
+				<a href="https://github.com/organizations/spacehackersclub">Space Hackers on Github</a>
+				<ul>
+					<li>
+						Looking for ideas on how to organize{' '}
+						<a href="https://github.com/spacehackersclub/awesome-spacehackers">awesome-spacehackers</a>{' '}
+						list.
+					</li>
+				</ul>
+			</li>
+			<li>
+				Join us on <a href="https://keybase.io/">Keybase</a> to chat. Join the team <code>spacehackers</code>
+			</li>
+		</ul>
+	</div>
 );
 
 export default IndexPage;
