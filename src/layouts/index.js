956d89c71aef1446cb4ac63a13e6c4db36f0c3bd
Kevin B. Ridgway <kridgway@gmail.com>
2019-07-09 00:41:27 -0400

    Fixed version of gatsby and deps.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
+import { graphql } from 'gatsby';
 
 const TemplateWrapper = ({ children, data }) => (
 	<div>
